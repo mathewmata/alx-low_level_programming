366e24f7acded95108cc904cf0ec287f0c99750b
martha <matthewmartha23@gmail.com>
2023-03-16 19:53:33 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints using printf
- * Return: completed (0)
+ * main - Entry point
+ * Return: always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	char a,
+	int b;
+	long int d;
+	long long int e;
+	float f;
+
+printf("Size of a char: %lu byte(s)\n", (unsighned long)sizeof(a));
+printf("Size of a int: %lu byte(s)\n", (unsighned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsighned long)sizeof(d));
+printf("Size of a long long int: %lu byte(s)\n", (unsighned long)sizeof(e));
+printf("Size of a float: %lu byte(s)\n", (unsighned long)sizeof(f));
+return (0);
+}
