e99c682f9f603f14e0c8d83e4f1062e16e107566
martha <matthewmartha23@gmail.com>
2023-03-17 21:48:41 -0700

    Print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,9 +11,9 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0 ; i < 98 ; i++)
+	for (i = 0 ; i <= 98 ; i++)
 	{
-		for (j = 1 ; j < 99 ; j++)
+		for (j = i + 1 ; j <= 99 ; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
@@ -21,10 +21,10 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 			if (i == 98 && j == 99)
-			{
+			continue;
 				putchar(',');
 				putchar(' ');
-			}
+		}
 	}
 	putchar('\n');
 	return (0);
