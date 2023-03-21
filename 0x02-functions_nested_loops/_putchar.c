7f05cf6d385c9adef766054a628170ef143ae137
martha <matthewmartha23@gmail.com>
2023-03-21 16:14:41 -0700

    Putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -7,7 +7,7 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int _putcahr(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putcahr(char c);
+int _putchar(char c);
 
 #endif
