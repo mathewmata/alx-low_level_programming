5a974396f3949539f031260fff94537dd16006b5
martha <matthewmartha23@gmail.com>
2023-04-10 21:55:42 -0700

    0x09 static libraries

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies bytes from memory area to memory area
+ * @n: number of bytes to be copied
+ * @src: memory copied
+ * @dest: memory stored
+ *
+ * Return: copied memory with n byted changed
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int j = 0;
+	int k = n;
+
+	for (; j < k; j++)
+	{
+		dest[j] = src[j];
+		n--;
+	}
+	return (dest);
+}
